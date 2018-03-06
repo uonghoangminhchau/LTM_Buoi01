4c627629cddb4b4e3768c33db9c1d93c5390aa32
Chau <you@example.com>
2018-03-06 17:29:12 +0700

    Done

---
+++ BT01/BT01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace BT01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Get host name
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+
+            // Find host by name
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Enumerate IP addresses
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +
+                                  ipaddress.ToString());
+            }
+        }
+    }
+}
---
+++ BT02/BT02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace BT02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien hoac ip: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.Resolve(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+
+            // Danh sách điạ chỉ IP
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t+ IP: {0}", i);
+        }
+    }
+}
